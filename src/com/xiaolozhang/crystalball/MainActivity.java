b07c0bf9d96b8637e38def9d62274051cdcb8b26
Zhang, Xiaolong <xlzhsteven@gmail.com>
2013-12-26 22:51:01 -0800

    Crystal Ball
    Adding code logic to random generate integer from 0 to 2

---
+++ src/com/xiaolozhang/crystalball/MainActivity.java
@@ -0,0 +1,43 @@
+package com.xiaolozhang.crystalball;
+
+import java.util.Random;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		// Declare our new variable
+		final TextView answerLabel = (TextView) findViewById(R.id.textView1);
+		Button getAnswerButton = (Button) findViewById(R.id.button1);
+		getAnswerButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				String answer = "";
+				Random randomGenerator = new Random();
+				int randomNumber = randomGenerator.nextInt(3);
+				answer = Integer.toString(randomNumber);
+				answerLabel.setText(answer);
+			}
+		});
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
