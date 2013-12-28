c32fd9a377836e62fb1b1a11a12c6513bd1159ab
Zhang, Xiaolong <xiaolozhang@paypal.com>
2013-12-27 22:25:44 -0800

    Create Crystal ball class with its member variable and methods and use it in the MainActivity.java file

--- src/com/xiaolozhang/crystalball/MainActivity.java
+++ src/com/xiaolozhang/crystalball/MainActivity.java
@@ -1,7 +1,5 @@
 package com.xiaolozhang.crystalball;
 
-import java.util.Random;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,7 +8,10 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	// member variable(s)
+	private CrystalBall mCrystalBall = new CrystalBall();
 
+	// method(s)
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -22,21 +23,7 @@ public class MainActivity extends Activity {
 
 			@Override
 			public void onClick(View v) {
-				String[] answers = { "It is certain", "It is decidedly so",
-						"All signs say YES", "The stars are not aligned",
-						"My reply is no", "It is doubtful",
-						"Better not tell you now", "Concentrate and ask again",
-						"Unable to answer now" };
-
-				// Create variable answer to store empty string
-				String answer = "";
-
-				// Random generator
-				Random randomGenerator = new Random();
-
-				// Generate integer from 0 to the length of the answers array
-				int randomNumber = randomGenerator.nextInt(answers.length);
-				answer = answers[randomNumber];
+				String answer = mCrystalBall.getAnAnswer();
 
 				// Set variable answer to answerLabel
 				answerLabel.setText(answer);
